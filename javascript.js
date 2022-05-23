7f5875a63562f179ab689440210706622fac9d5b
Accellerant <accellerant@gmail.com>
2022-05-23 15:21:07 -0700

    ++Operate +div +bParam
    ++Created the Operate function:
        Evaluate how to process the provided
        variables with the "operator" string.
    
    +Updated div:
        Changed the function name: divide > div.
    
    +Updated bParam:
        Shortened the description for the function.

--- javascript.js
+++ javascript.js
@@ -19,17 +19,44 @@ function mult (a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function div (a, b) {
     if(!bParam(b))
         return a / a;
 
     return a / b;
 }
 
-//If the b parameter for the operators is not provided, return false.
+//If "b" for the operators isn't provided, return false.
 function bParam(b) {
     if(typeof(b) === "undefined")
         return false;
     
     return true;
+}
+
+/*
+Take in a string for operator, and pass the variables "a" and "b"
+to their appropriate function.
+*/
+function operate (operator, a, b) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        
+        case "-":
+            sub(a, b);
+            break;
+
+        case "*":
+            mult(a, b);
+            break;
+
+        case "/":
+            div(a, b);
+            break;
+
+        default:
+            console.log("ERROR: No provided Operator!!!!");
+    }
 }
