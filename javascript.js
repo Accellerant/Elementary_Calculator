1928eb078b996fec52cfeda8672fb474348df538
Accellerant <accellerant@gmail.com>
2022-06-07 17:46:54 -0700

    Bug fixes & anonFunct > functions
    === misc bug fixes ===
    
    @roundDecimal
    	return a   >   return Number(a)
    		The value being returned would sometimes go back as a string
            which would cause some future calculations to have
            an extra digit.
    
    @calcBtnOperation
    	else if (userNum != "" && userNum2 != "")
    		V
    	else if (userNum != "" && userNum2 != "" && currentNum != "")
    		At times, userNum2 would be given an empty string which would
    		mess up calculations.
    
    	if(currentNum != "")
    		Previously, when different operators were chosen but a
    		calculation doesn't take place, an empty string would be
    		displayed on the screen. This rectifies this.
    
    	currentNum = 0   >   currentNum = ""
    		When cycling through the operators on the calculator, the zero
    		would be taken into effect after hitting the division button.
    		This also previously broke being able to self calculate when
    		only a single number and an operator is chosen, followed by
    		the equal button.
    
    === Anonymous Functions > Functions ====
    Because I want to add keyboard support to the page, the anonymous
    functions for the buttons needs to be updated to stand alone functions.
    
    calcBtnOperation()
    	opButtons Anonymous function for each button has been converted
    	to its own function. The only addition to this, outside of
    	what's mentioned in the bug fixes, is the changing of the following
    	assignment:
    
    	userOperation = button.textContent
            V
        userOperation = this.textContent
    
    calcBtnSolve()
    	calcButton's anonymous function was updated to a regular function.

--- javascript.js
+++ javascript.js
@@ -90,13 +90,12 @@ function roundDecimal(a) {
             if(decNum[decPos + 1] == "0" && decNum[decPos +2] == "0")
                 decNum = "0";
         }
-            
-
         a = Number(wholeNum) + Number(decNum);
-        
     }
 
-    return a;
+    
+
+    return Number(a);
     
 
 }
@@ -116,6 +115,8 @@ function resetMainVals() {
     userOperation = "";
     calculated = false;
 }
+
+
 
 let userNum1 = "", userNum2 = "", userOperation = "";
 let currentNum = "";
@@ -155,32 +156,39 @@ clearButton.addEventListener('click', () => {
 
 const opButtons = document.querySelectorAll('[id^="btnOp_"]');
 opButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if(userOperation === "") {
-            userNum1 = Number(currentNum);
-            
-        } else if(calculated) {
-            currentNum = String(userNum1);
-            calculated = false;
-            userNum2 = "";
-        } else if(userNum1 != "" && userNum2 != ""){
-            userNum2 = Number(currentNum);
-            currentNum = userNum1 = roundDecimal(operate(userOperation, userNum1, userNum2));
-            
-        }
-
-        userOperation = button.textContent;
-        if(userNum2 == "")
-            userNum2 = Number(currentNum);
-            
-        populateDisplay(currentNum);
-        currentNum = "0";
-        decimalUsed = false;
-    });
+    button.addEventListener('click', calcBtnOperation,);
 });
 
+function calcBtnOperation() {
+    if(userOperation === "") {
+        userNum1 = Number(currentNum);
+        
+    } else if(calculated) {
+        currentNum = String(userNum1);
+        calculated = false;
+        userNum2 = "";
+    } else if(userNum1 !== "" && userNum2 !== "" && currentNum !== ""){
+        userNum2 = Number(currentNum);
+        currentNum = userNum1 = roundDecimal(operate(userOperation, userNum1, userNum2));
+        
+    }
+
+    userOperation = this.textContent;
+
+    if(userNum2 == "")
+        userNum2 = Number(currentNum);
+        
+    if(currentNum != "")
+        populateDisplay(currentNum);
+
+    currentNum = "";
+    decimalUsed = false;
+}
+
 const calcButton = document.querySelector('#btnCalc');
-calcButton.addEventListener('click', () => {
+calcButton.addEventListener('click', calcBtnSolve, false);
+
+function calcBtnSolve() {
     if(userOperation === "") {
         populateDisplay(currentNum);
     } else {
@@ -198,5 +206,5 @@ calcButton.addEventListener('click', () => {
             currentNum = "";
         } 
     }
+}
 
-});
